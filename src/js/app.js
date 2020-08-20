2aa2d5b047faf2e6b50fdc05f49ffb8cc29272ce
rirtakmanna <web@rirtakmanna.com>
2020-08-20 20:26:07 +0530

    Complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import Chart from "chart.js";
 
-var canvas = document.getElementsByTagName("canvas");
-
+// funtion for load JSON
 function loadJSON(url, callback) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
@@ -15,36 +14,68 @@ function loadJSON(url, callback) {
   xobj.send(null);
 }
 
+// Each graph doing
+var canvas = document.getElementsByTagName("canvas");
 Array.prototype.forEach.call(canvas, (chart) => {
   var type = `${chart.getAttribute("data-graph")}`;
   var dataJSON = `./${chart.getAttribute("data-JSON")}.json`;
+  var lebel = `${chart.getAttribute("data-label")}`;
+  lebel = lebel.split(",");
 
-  var option = {
-    animation: false,
-    maintainAspectRatio: false,
-    responsive: true,
-    scales: {
-      xAxes: [
-        {
-          stacked: true,
-        },
-      ],
-      yAxes: [
-        {
-          stacked: true,
-        },
-      ],
-    },
-  };
+  var option, bcColor;
+
+  if (chart.getAttribute("data-graph") == "line") {
+    option = {
+      maintainAspectRatio: false,
+      animation: false,
+      responsive: true,
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+      },
+    };
+
+    bcColor = "rgba(60,141,188,0.9)";
+  } else if (chart.getAttribute("data-graph") == "doughnut") {
+    option = {
+      maintainAspectRatio: false,
+      responsive: true,
+      animation: false,
+    };
+    bcColor = [
+      "#f56954",
+      "#00a65a",
+      "#f39c12",
+      "#00c0ef",
+      "#3c8dbc",
+      "#d2d6de",
+    ];
+  }
 
   loadJSON(dataJSON, function (response) {
     var data = JSON.parse(response);
     // console.log(type);
     var graphData = {
-      labels: data.label,
+      labels: lebel,
       datasets: [
         {
           data: data.data,
+          backgroundColor: bcColor,
         },
       ],
     };
@@ -59,10 +90,11 @@ Array.prototype.forEach.call(canvas, (chart) => {
     loadJSON(dataJSON, function (response) {
       var data = JSON.parse(response);
       var graphData = {
-        labels: data.label,
+        labels: lebel,
         datasets: [
           {
             data: data.data,
+            backgroundColor: bcColor,
           },
         ],
       };
@@ -73,4 +105,27 @@ Array.prototype.forEach.call(canvas, (chart) => {
       });
     });
   }, 20000);
+});
+
+var show = document.getElementsByClassName("show");
+Array.prototype.forEach.call(show, (e) => {
+  e.addEventListener("click", (event) => {
+    var cardElement = event.target.parentElement.parentElement.parentElement;
+
+    var cardBody = cardElement.getElementsByClassName("card__body")[0];
+
+    if (cardBody.style.display === "none") {
+      cardBody.style.display = "block";
+    } else {
+      cardBody.style.display = "none";
+    }
+  });
+});
+
+var close = document.getElementsByClassName("close");
+Array.prototype.forEach.call(close, (e) => {
+  e.addEventListener("click", (event) => {
+    var cardElement = event.target.parentElement.parentElement.parentElement;
+    cardElement.style.display = "none";
+  });
 });
